a409633edac855cd7fdc8d351349b78f30fb9fbe
hooless <hoogle349919751@gmail.com>
2015-08-20 16:37:50 +0800

    add comment

--- vim.sh
+++ vim.sh
@@ -1,5 +1,6 @@
 #! /bin/bash 
 
+[[ ! -f vimrc  ]] && echo "Make sure you are in the directory which has vimrc" && exit -1 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vimrc ~/
 
