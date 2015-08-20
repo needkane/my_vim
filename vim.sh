82480bf056e7f58ac9c4b359c307d0edda734343
hooless <hoogle349919751@gmail.com>
2015-08-20 16:46:30 +0800

    more userful

--- vim.sh
+++ vim.sh
@@ -1,7 +1,10 @@
 #! /bin/bash 
 
-[[ ! -f vimrc  ]] && echo "Make sure you are in the directory which has vimrc" && exit -1 
+[[ ! -f ./vimrc  ]] && echo "Make sure you are in the directory which has vimrc" && exit -1 
+[[ -f ~/.vimrc  ]] && mv ~/.vimrc ~/vimrc.bak
+[[ ! -d ~/.vim/bundle ]] && mkdir -p ~/.vim/bundle
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./vimrc ~/
+ln  -sv `pwd`/vimrc ~/.vimrc
 
 echo -e "Done ! \nPlease  Launch vim and run :PluginInstall"
